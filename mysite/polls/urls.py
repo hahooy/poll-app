e66ce9248437ff257010b2ccb90fd9744f2b7009
Yongzheng Huang <iamhuangyz@gmail.com>
2015-12-26 14:37:20 -0500

    added html template

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [url(r'^$', views.index, name='index'), url(r'^hello/$', views.hello, name='hello')]
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^hello/$', views.hello, name='hello'),
+]
